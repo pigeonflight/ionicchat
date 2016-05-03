2195a8e6ab6c381ae4dcdec81438f0c9b0b6fc05
David Bain <pigeonflight@gmail.com>
2016-05-03 20:35:50 +0000

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,60 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('GeoCtrl',['$scope','$ionicPlatform','$cordovaGeolocation',
+      '$ionicLoading','$timeout',function($scope, $ionicPlatform,$cordovaGeolocation,$ionicLoading,$timeout){
+        $ionicPlatform.ready(function(){
+          $scope.modal = $ionicLoading.show({
+            content: 'Fetching Current Location....',
+            showBackdrop: false
+          });
+          var posOptions = {
+            timeout: 10000,
+            enableHighAccuracy: false
+            
+          };
+          $cordovaGeolocation
+                    .getCurrentPosition(posOptions)
+                    .then(function(position) {
+                      $scope.latitude = position.coords.latitude;
+                      $scope.longitude = position.coords.longitude;
+                      $scope.accuracy = position.coords.accuracy;
+                      $scope.dataReceived = true;
+                      $scope.modal.hide();
+                    }, function(err) {
+                      // error
+                      $scope.modal.hide();
+                      $scope.modal = $ionicLoading.show({
+                        content: 'Oops!! ' + err,
+                        showBackdrop: false
+                      });
+                      $timeout(function() {
+                        $scope.modal.hide();
+                      }, 3000);
+                    });
+          
+          });
+        
+        
+      }])
